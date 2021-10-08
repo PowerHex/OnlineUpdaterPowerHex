075d542c03c1c927c3a37d866067712b18fa2db0
PowerHex <92164041+PowerHex@users.noreply.github.com>
2021-10-08 20:00:41 +0700

    Create powerhaxupdater.cpp

---
+++ powerhaxupdater.cpp
@@ -0,0 +1,9 @@
+{
+"Version": 1,
+"Creator": "PowerHax",
+"Title": "Beta",
+"Info": "Launcher Feature\n- Support Growtopia Sound\n- Using Growtopia 32bit\n- Minim Size (10 MB) only",
+"Changelogs": "Official Youtube ↓↓",
+"Link": "https://youtube.com/c/PowerHax",
+"Premium": false
+}
